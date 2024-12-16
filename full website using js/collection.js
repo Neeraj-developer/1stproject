eb28c17f2e9349e393ce1025302782cc5aa9682c
Neeraj-developer <developer.neeeraj.0402@gmail.com>
2024-12-17 01:39:10 +0530

    chnage background color and svg are include remove some icon

--- full website using js/collection.js
+++ full website using js/collection.js
@@ -1,4 +1,4 @@
-  // Product Data Array
+// Product Data Array
 const products = [
     {
         name: "Red Men Shoes",
@@ -36,7 +36,7 @@ const products = [
 const container = document.getElementById("productContainer");
 
 // Loop through Products Array and Generate HTML
-products.forEach((product) => {
+products.forEach((product, index) => {
     const productCard = document.createElement("div");
     productCard.classList.add("coll_box");
 
@@ -45,7 +45,7 @@ products.forEach((product) => {
             <img src="${product.image}" alt="${product.name}">
             <button class="cart_animate">
                 <svg width="28px" height="28px" viewBox="0 0 1 1" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.957 0.3H0.257L0.2 0.067H0.043v0.033h0.131l0.162 0.67a0.083 0.083 0 0 0 -0.06 0.08 0.083 0.083 0 1 0 0.167 0c0 -0.019 -0.006 -0.036 -0.017 -0.05h0.234a0.083 0.083 0 0 0 -0.017 0.05 0.083 0.083 0 1 0 0.083 -0.083H0.37l-0.016 -0.067h0.468L0.957 0.3zM0.41 0.85c0 0.028 -0.022 0.05 -0.05 0.05s-0.05 -0.022 -0.05 -0.05 0.022 -0.05 0.05 -0.05 0.05 0.022 0.05 0.05m0.367 0a0.05 0.05 0 0 1 -0.1 0 0.05 0.05 0 0 1 0.1 0m-0.431 -0.183L0.265 0.333h0.646L0.797 0.667z" fill="#000"></path>
+                    <path d="M0.957 0.3H0.257L0.2 0.067H0.043v0.033h0.131l0.162 0.67a0.083 0.083 0 0 0 -0.06 0.08 0.083 0.083 0 1 0 0.167 0c0 -0.019 -0.006 -0.036 -0.017 -0.05h0.234a0.083 0.083 0 0 0 -0.017 0.05 0.083 0 1 0 0.083 -0.083H0.37l-0.016 -0.067h0.468L0.957 0.3zM0.41 0.85c0 0.028 -0.022 0.05 -0.05 0.05s-0.05 -0.022 -0.05 -0.05 0.022 -0.05 0.05 -0.05 0.05 0.022 0.05 0.05m0.367 0a0.05 0.05 0 0 1 -0.1 0 0.05 0.05 0 0 1 0.1 0m-0.431 -0.183L0.265 0.333h0.646L0.797 0.667z" fill="#000"></path>
                 </svg>
             </button>
             <div class="quick_btn">Quick View</div>
@@ -53,7 +53,7 @@ products.forEach((product) => {
         <div class="pro_info">
             <div class="box1">
                 <span>${product.name}</span>
-                <i class="fa-regular fa-heart" id="fav_icon"></i>
+                <i class="fa-regular fa-heart fav_icon" id="fav_icon_${index}"></i>
             </div>
             <p>${product.price}</p>
         </div>
@@ -64,11 +64,10 @@ products.forEach((product) => {
 });
 
 // mouseover effect slider
-
 const proBox = document.querySelectorAll('.coll_box');
 const cartAnimate = document.querySelectorAll('.cart_animate');
 const quickLinkBtn = document.querySelectorAll('.quick_btn');
-const fav_icon = document.querySelectorAll('#fav_icon');
+const fav_icon = document.querySelectorAll('.fav_icon');
 
 // Hover effect in product box
 if (proBox && quickLinkBtn) {
@@ -83,14 +82,58 @@ if (proBox && quickLinkBtn) {
             cartAnimate[index]?.classList.remove('transfor_cart');
         });
     });
-};
+}
 
-if (proBox && fav_icon) {
-    proBox.forEach((icon, index) => {
+// Handle clicking the favorite icon
+if (fav_icon) {
+    fav_icon.forEach((icon, index) => {
         icon.addEventListener('click', () => {
-            fav_icon.classList.remove('fa-regular');
-            fav_icon.classList.add('fa-solid');
+            // Get product info to save to localStorage
+            const product = products[index];
+            let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+            // Check if the product is already in favorites
+            const isFavorited = favorites.some(fav => fav.name === product.name);
+
+            if (isFavorited) {
+                // Remove from favorites if already added
+                favorites = favorites.filter(fav => fav.name !== product.name);
+                console.log(`${product.name} removed from favorites`);
+
+                // Update icon
+                icon.classList.remove('fa-solid');
+                icon.classList.add('fa-regular');
+            } else {
+                // Add to favorites if not already added
+                favorites.push(product);
+                console.log(`${product.name} added to favorites`);
+
+                // Update icon
+                icon.classList.remove('fa-regular');
+                icon.classList.add('fa-solid');
+            }
+
+            // Save updated favorites array to localStorage
+            localStorage.setItem('favorites', JSON.stringify(favorites));
         });
     });
 }
 
+// Optional: Load favorite state on page load
+document.addEventListener("DOMContentLoaded", () => {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    
+    fav_icon.forEach((icon, index) => {
+        const product = products[index];
+        const isFavorited = favorites.some(fav => fav.name === product.name);
+
+        // Set the icon to solid (filled heart) if product is in favorites
+        if (isFavorited) {
+            icon.classList.remove('fa-regular');
+            icon.classList.add('fa-solid');
+        } else {
+            icon.classList.remove('fa-solid');
+            icon.classList.add('fa-regular');
+        }
+    });
+});
